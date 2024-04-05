a95134e22782032bdaeb6c2d58d9d72956d98d5e
NGOC VY <NGOC VY@MSI>
2024-04-05 21:36:38 +0700

    huy ngu

--- QuanLyBanSach/Program.cs
+++ QuanLyBanSach/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyBanSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmTaotk());
+            Application.Run(new frmDangnhap());
         }
     }
 }
